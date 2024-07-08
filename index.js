75080dbea4cb9b865d507e2aa74ceb7a8dd27aae
savannahmarshall <savvymarshall@gmail.com>
2024-07-08 14:05:07 -0700

    added example svg logo

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
+
 const questions = [
   {
     type: 'input',
     name: 'text',
-    message: 'Enter up to three characters:',
+    message: 'Please enter up to three characters:',
     validate: input => input.length <= 3 || 'Please enter up to three characters.',
   },
   {
     type: 'input',
     name: 'textColor',
-    message: 'Enter a color keyword or hexadecimal number for the text color:',
+    message: 'Please enter the color you would like your text to be:',
   },
   {
     type: 'list',
     name: 'shape',
-    message: 'Choose a shape:',
+    message: 'Please choose a shape:',
     choices: ['Circle', 'Triangle', 'Square'],
   },
   {
     type: 'input',
     name: 'shapeColor',
-    message: 'Enter a color keyword or hexadecimal number for the shape color:',
+    message: 'Please enter your desired shape color:',
   }
 ];
 
 inquirer.prompt(questions).then(answers => {
     let shapeInstance;
+  
     switch (answers.shape) {
       case 'Circle':
         shapeInstance = new Circle();
@@ -40,15 +42,14 @@ inquirer.prompt(questions).then(answers => {
         shapeInstance = new Square();
         break;
     }
-
   shapeInstance.setColor(answers.shapeColor);
 
   const svgContent = `
-    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      ${shapeInstance.render()}
-      <text x="150" y="125" font-size="40" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-    </svg>
-  `;
+  <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${shapeInstance.render()}
+    <text x="150" y="110" font-size="40" font-weight="bold" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+  </svg>
+`;
 
   fs.writeFile('./examples/logo.svg', svgContent.trim(), err => {
     if (err) {
